ce60769ad19f431be49bd2c0b56f56a803b8b466
Joey <yousef.yas0080@gmail.com>
2025-05-15 20:25:00 +0330

    Define API routes

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ShebaController;
+
+// Submit Sheba transfer request
+Route::post( '/sheba', [ ShebaController::class, 'store' ] );
+
+// Get all Sheba transfer requests
+Route::get( '/sheba', [ ShebaController::class, 'index' ] );
+
+// Confirm or cancel Sheba transfer request
+Route::put( '/sheba/{id}', [ ShebaController::class, 'update' ] );
+Route::post( '/sheba/{id}', [ ShebaController::class, 'update' ] ); // Alternative method for the same action
